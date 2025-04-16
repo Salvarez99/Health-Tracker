1e0d5348ee7d70003a8acf73e04240a1fd44c1f3
Salvarez99 <stephenalvarez3@gmail.com>
2025-04-16 17:49:40 -0400

    Sign in/up Firebase Auth Implemented

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,11 +6,12 @@ import { lightTheme } from "@/themes/LightTheme";
 import { Stack, Tabs } from "expo-router";
 import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
-// import UnitsToggle from "../components/UnitsToggle";
 import * as Local from "../localDB/InitializeLocal";
+import auth, {FirebaseAuthTypes} from "@react-native-firebase/auth";
 
 export default function RootLayout() {
   const [theme, setTheme] = useState(lightTheme);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
 
   const headerStyle = {
     backgroundColor: theme.colors.tertiary,
@@ -31,8 +32,16 @@ export default function RootLayout() {
     }
   };
 
+  const onAuthStateChanged = (user: FirebaseAuthTypes.User | null) => {
+    console.log("User state changed:", user);
+    setUser(user);
+  }
+
   useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged)
     getUserPrefs();
+
+    return subscriber; // unsubscribe on unmount
   }, []);
 
   return (
