98210bbf304b1603f9353be81c9de2ba91c04819
Salvarez99 <stephenalvarez3@gmail.com>
2025-04-23 14:00:26 -0400

    Fix RNC Material DatePicker issue

--- services/api.tsx
+++ services/api.tsx
@@ -1,5 +1,5 @@
 import { createApi }  from "@reduxjs/toolkit/query/react"
-import { fetchBaseQuery } from "@reduxjs/toolkit/query"
+import { fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://trackapi.nutritionix.com",
